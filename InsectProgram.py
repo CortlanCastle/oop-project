ae6bc5c8dc13b38bdc64f54d6ba20291ad652c80
Cortlan <Cortlan_castle1@baylor.edu>
2024-02-21 14:11:43 -0600

    Worked on oop

---
+++ InsectProgram.py
@@ -0,0 +1,10 @@
+import insectClass as i
+
+mosquito = i.Insect()
+housefly = i.Insect()
+
+mosquito.calc_flight()
+housefly.calc_flight()
+
+print(f"the mosquito can fly up to {mosquito.get_miles()}")
+print(f"the housefly can fly up to {housefly.get_miles()}")
